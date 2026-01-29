2e5aec2efd1eb35ad79c3eb57cdbe1212a9cace5
topimmo <contact@topaffaireimmo.com>
2026-01-29 15:50:36 +0100

    Refactor Home component to remove Header and Footer
    Removed Header and Footer components from Home.

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,25 +1,16 @@
-import Header from "@/components/layout/Header";
-import Footer from "@/components/layout/Footer";
 import HeroSearch from "@/components/home/HeroSearch";
 import FeaturedProperties from "@/components/home/FeaturedProperties";
-import AdBanner from "@/components/home/AdBanner";
 import LatestListings from "@/components/home/LatestListings";
 import ExploreByCityMap from "@/components/home/ExploreByCityMap";
 
 function Home() {
   return (
-    <div className="min-h-screen flex flex-col">
-      <Header />
-      <main className="flex-1">
-        <HeroSearch />
-        <FeaturedProperties />
-        <AdBanner page="home" position="after_featured" />
-        <LatestListings />
-        <ExploreByCityMap />
-        <AdBanner page="home" position="before_footer" className="bg-muted/30" />
-      </main>
-      <Footer />
-    </div>
+    <main className="flex-1">
+      <HeroSearch />
+      <FeaturedProperties />
+      <LatestListings />
+      <ExploreByCityMap />
+    </main>
   );
 }
 
